a49d0ba2c163cd1dd37db21a8272c0d173c9ea3a
GeloFunz <gelomartinfunelas@gmail.com>
2025-02-18 12:15:00 +0800

    Added index and recipe views

--- ledger/views.py
+++ ledger/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def recipe(request, recipe_index):
+    return render(request, "recipe.html")
