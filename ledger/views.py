44a294f7ef3094ca46858e6c8e4dd708895c55f8
GeloFunz <gelomartinfunelas@gmail.com>
2025-02-18 12:20:58 +0800

    Updated views to pass recipe data into context

--- ledger/views.py
+++ ledger/views.py
@@ -1,8 +1,76 @@
 from django.shortcuts import render
 
+RECIPE_DATA = [
+        {
+            "name": "Recipe 1",
+            "ingredients": [
+                {
+                    "name": "tomato",
+                    "quantity": "3pcs"
+                },
+                {
+                    "name": "onion",
+                    "quantity": "1pc"
+                },
+                {
+                    "name": "pork",
+                    "quantity": "1kg"
+                },
+                {
+                    "name": "water",
+                    "quantity": "1L"
+                },
+                {
+                    "name": "sinigang mix",
+                    "quantity": "1 packet"
+                }
+            ],
+            "link": "/recipe/1"
+        },
+        {
+            "name": "Recipe 2",
+            "ingredients": [
+                {
+                    "name": "garlic",
+                    "quantity": "1 head"
+                },
+                {
+                    "name": "onion",
+                    "quantity": "1pc"
+                },
+                {
+                    "name": "vinegar",
+                    "quantity": "1/2cup"
+                },
+                {
+                    "name": "water",
+                    "quanity": "1 cup"
+                },
+                {
+                    "name": "salt",
+                    "quantity": "1 tablespoon"
+                },
+                {
+                    "name": "whole black peppers",
+                    "quantity": "1 tablespoon"
+                },
+                {
+                    "name": "pork",
+                    "quantity": "1 kilo"
+                }
+            ],
+            "link": "/recipe/2"
+        }
+    ]
+
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    return render(request, "index.html", {
+        "recipes": RECIPE_DATA
+    })
 
 def recipe(request, recipe_index):
-    return render(request, "recipe.html")
+    return render(request, "recipe.html", {
+        "recipe_index": recipe_index,
+        "recipe": RECIPE_DATA[recipe_index-1]
+    })
