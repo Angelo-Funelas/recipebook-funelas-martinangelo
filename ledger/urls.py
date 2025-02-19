f9e22191e9b83264d1d96f67305da1d8452aaff3
GeloFunz <gelomartinfunelas@gmail.com>
2025-02-19 22:48:43 +0800

    Updated urls to match specifications

--- ledger/urls.py
+++ ledger/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("list", views.index, name="index"),
-    path("<int:recipe_index>", views.recipe, name="recipe")
+    path("recipes/list", views.index, name="index"),
+    path("recipe/<int:recipe_index>", views.recipe, name="recipe")
 ]
