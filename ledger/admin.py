7b768e838b015fbe858ade4117d30f6ad5ddcdcc
Angelo-Funelas <angelomartinfunelas@gmail.com>
2025-02-24 14:26:53 +0800

    Registered models to admin

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+    search_fields = ('name',)
+    list_display = ('name',)
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    model = RecipeIngredient
+    list_display = ('Ingredient', 'Recipe', 'Quantity')
+    list_filter = ('Recipe', 'Ingredient')
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 0
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    search_fields = ('name',)
+    list_display = ('name',)
+    inlines = [RecipeIngredientInline]
+    
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
