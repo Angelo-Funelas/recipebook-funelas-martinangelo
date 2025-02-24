b2a43302763cde3e31c21d1ef29690ec757cd913
Angelo-Funelas <angelomartinfunelas@gmail.com>
2025-02-24 14:26:24 +0800

    Added models

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=64)
+    def __str__(self):
+        return self.name
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=64)
+    def __str__(self):
+        return self.name
+    
+class RecipeIngredient(models.Model):
+    Quantity = models.IntegerField(default=1)
+    Ingredient = models.ForeignKey(Ingredient, on_delete=models.PROTECT, related_name="ingredient")
+    Recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="recipe")
+    def __str__(self):
+        return self.Ingredient.name
