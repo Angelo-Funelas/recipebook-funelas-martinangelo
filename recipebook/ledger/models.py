32d4c2c0620d60b54b7e0376bfd83120d0afae2f
Angelo-Funelas <angelomartinfunelas@gmail.com>
2025-03-10 19:15:10 +0800

    Added author, createdOn, and updatedOn fields to Recipe model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -3,6 +3,11 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.TextField(max_length=255, null=True)
+    
 class Ingredient(models.Model):
     name = models.CharField(max_length=64)
     def __str__(self):
@@ -10,6 +15,9 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=64)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="recipes")
+    createdOn = models.DateTimeField(auto_now_add=True)
+    updatedOn = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.name
     def get_absolute_url(self):
@@ -21,8 +29,3 @@ class RecipeIngredient(models.Model):
     Recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="ingredients")
     def __str__(self):
         return self.Ingredient.name
-    
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
-    bio = models.TextField(max_length=255, null=True)
