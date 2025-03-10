32d326a486899e1ff64c56ec61ae009348324142
Angelo-Funelas <angelomartinfunelas@gmail.com>
2025-03-10 20:28:01 +0800

    Added login requirement to list and details page

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import *
 
 # Create your views here.
+@login_required
 def index(request):
     return render(request, "index.html", {
         "recipes": Recipe.objects.all()
     })
 
+@login_required
 def recipe(request, recipeID):
     recipe = Recipe.objects.get(id=recipeID)
     return render(request, "recipe.html", {
